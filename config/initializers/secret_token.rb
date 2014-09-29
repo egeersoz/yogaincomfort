5e1090fad3db9c9130542a403d3fb65381e7c77f
Ege Ersoz <ege.ersoz@gmail.com>
2014-09-28 19:17:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yogaincomfort::Application.config.secret_key_base = '0468fa072b2546a0cb83b1f0b5e9d51bac364b0fdac0d98245eba32db1ea51689b1b86444858078b495d5fa67d88aeb8d4dc3a54b1a3da55764dc0ceb95dce2e'
